9253d4c01dc86a7231acbb912fb4618a18931eac
Suecia76 <carrascosanti76@gmail.com>
2025-05-26 11:37:12 -0300

    agrego vistas con sus urls y un template inicial

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def saludar(request):
+    return HttpResponse("¡Hola, mundo!")
+
+def saludar_con_etiqueta(request):
+    return HttpResponse("<h1 style='color:red;'>¡Hola, mundo!</h1>")
+
+def saludar_con_parametros(request, nombre:str,apellido:str):
+    return HttpResponse(f"<h1 style='color:blue;'>¡Hola, {nombre} {apellido}!</h1>")
+
+def index(request):
+    return render(request, 'core/index.html')
